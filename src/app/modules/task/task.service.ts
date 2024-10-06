56c5689fb3a51a9b2f24857a8a4e30833cd47299
Hridoy Islam <hridoy4t@gmail.com>
2024-10-06 23:03:26 +0600

    populate add

--- src/app/modules/task/task.service.ts
+++ src/app/modules/task/task.service.ts
@@ -12,7 +12,7 @@ const createTaskIntoDB = async (payload: TTask) => {
 
 const getAllTaskFromDB = async (query: Record<string, unknown>) => {
   const taskQuery = new QueryBuilder(
-    Task.find().populate("author").populate("assigned"),
+    Task.find().populate("author assigned company"),
     query
   )
     .search(TaskSearchableFields)
